86ac7f543e0c2d395de56b5a03e9fdccb348d16f
Gabana <gabana@Gabanas-MacBook-Pro.local>
2026-03-12 22:23:55 +0300

    Configure Tenant model and custom columns

---
+++ app/Models/Tenant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Contracts\Tenancy\Database\Models\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+
+class Tenant extends BaseTenant implements TenantWithDatabase
+{
+    use HasDatabase, HasDomains;
+
+    public static function getCustomColumns(): array
+    {
+        return [
+            'id',
+            'name',
+            'owner_name',
+            'owner_phone',
+            'owner_whatsapp',
+            'plan',
+            'status',
+        ];
+    }
+}
